887451caa69c40555cbe01cc05a2766c86685608
Janith Kulatunge <janith.kulatunge@tufts.edu>
2019-12-28 17:13:46 -0500

    visible area capture

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Chrome Screenshot Extension 
 ### December 2019
+
+Used https://stackoverflow.com/questions/29275409/chrome-extension-automatically-download-a-screenshot-taken-with-chrome-tabs-ca
+ 
---
+++ background.js
@@ -0,0 +1,39 @@
+  var screenshot = {
+  	content : document.createElement("canvas"),
+  	data : '',
+
+ 	init : function() {
+    	this.initEvents();
+  	},
+
+ 	saveScreenshot : function() {
+    	var image = new Image();
+    	image.onload = function() {
+      		var canvas = screenshot.content;
+      		canvas.width = image.width;
+      		canvas.height = image.height;
+      		
+      		var context = canvas.getContext("2d");
+      		context.drawImage(image, 0, 0);
+
+			// save the image
+			var link = document.createElement('a');
+			link.download = "download.png";
+			link.href = screenshot.content.toDataURL();
+			link.click();
+			screenshot.data = '';
+    		};
+    	image.src = screenshot.data; 
+  	},
+
+	initEvents : function() {
+		chrome.commands.onCommand.addListener(function(take_screenshot) {
+	          chrome.tabs.captureVisibleTab(null, {format : "png"}, function(data) {
+	                screenshot.data = data;
+	                screenshot.saveScreenshot();
+	            }); 
+	        });
+	    }
+	};
+
+screenshot.init();
